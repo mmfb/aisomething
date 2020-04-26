127ef007b201bb989a35e0073b059e24140aa20d
Miguel Bugalho <miguel.bugalho@gmail.com>
2020-04-26 18:11:34 +0100

    Dao to DAO

--- routes/tec_rout.js
+++ routes/tec_rout.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var authenDAO = require('../models/tecDao');
+var authenDAO = require('../models/tecDAO');
 
 router.get('/:tec', function (req, res, next) {
   var tec=req.params.tec
